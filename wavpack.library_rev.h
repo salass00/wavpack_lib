8f81fdfd34ce5aa020ec376b9d25154598488d21
Fredrik Wikstrom <fredrik@a500.org>
2018-09-16 11:20:55 +0300

    Bumped revision date for first release.

--- wavpack.library_rev.h
+++ wavpack.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
 #define REVISION	1
-#define DATE		"13.9.2018"
+#define DATE		"16.9.2018"
 #define VERS		"wavpack.library 53.1"
-#define VSTRING		"wavpack.library 53.1 (13.9.2018)\r\n"
-#define VERSTAG		"\0$VER: wavpack.library 53.1 (13.9.2018)"
+#define VSTRING		"wavpack.library 53.1 (16.9.2018)\r\n"
+#define VERSTAG		"\0$VER: wavpack.library 53.1 (16.9.2018)"
